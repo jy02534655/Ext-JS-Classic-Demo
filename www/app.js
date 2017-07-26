569c4b9cee56700cf6bbbb947e91c9ca8132703c
534502520@qq.com <534502520@qq.com>
2017-07-26 22:23:09 +0800

    注释调整

--- www/app.js
+++ www/app.js
@@ -9,6 +9,7 @@ Ext.application({
     stores: ['NavigationTree'],
     //自动引入app目录下所以的类，不需要在其他地方再次引入
     requires: ['app.*'],
+    //默认起始路由
     defaultToken: 'home',
     // 默认起始页面
     mainView: 'app.view.main.Box'
